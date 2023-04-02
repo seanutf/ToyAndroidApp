edb316914c8cd3c7475a3b5f7a3e4ebe37104efd
seanutf <seanutf@gmail.com>
2023-04-02 18:16:17 +0800

    init project

---
+++ app/src/main/java/com/seanutf/learn/toyandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.seanutf.learn.toyandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
